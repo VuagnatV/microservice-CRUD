cc522953ec0aecc13fe1b351373e64eadbeceb5f
VuagnatV <37271745+VuagnatV@users.noreply.github.com>
2023-05-26 17:34:50 +0200

    v1

---
+++ categories-service.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const axios = require('axios')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+app.use(bodyParser.json())
+
+let categories = [
+    { id: 1, name: "category 1" },
+    { id: 2, name: "category 2" },
+    { id: 3, name: "category 3" },
+    { id: 4, name: "category 4" },
+]
+
+app.get('/categories', async (req, res) => {
+    res.json(categories)
+})
+
+app.get('/categories/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const category = categories.find(category => category.id === id)
+
+    if (category) {
+        res.json(category)
+    } else {
+        res.status(404).json({ error: "not found" })
+    }
+})
+
+app.put('/categories/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = categories.findIndex(category => category.id === id)
+    const category = categories.find(category => category.id === id)
+    if (category) {
+        try {
+
+            const newCategory = {
+                id: req.body.id,
+                name: req.body.name,
+
+            }
+
+            categories[index] = newCategory
+
+            res.json(categories)
+
+        } catch (error) {
+            res.status(500).json({ error: "dsfsd" })
+        }
+    } else {
+        res.status(500).json({ error: "dsfffffsd" })
+    }
+})
+
+app.post('/categories', (req, res) => {
+    console.log(req.body)
+    const newCategory = {
+        id: req.body.id,
+        name: req.body.name,
+    }
+    categories.push(newCategory)
+    res.json(categories)
+})
+
+app.listen(5000, () => {
+    console.log(`Server running at 5000`)
+})
---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const axios = require('axios')
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.json())
+
+let books = [
+    { id: 1, tittle: "livre 1", authorId: 1, categoryId: 1 },
+    { id: 2, tittle: "livre 2", authorId: 2, categoryId: 2 },
+    { id: 3, tittle: "livre 3", authorId: 3, categoryId: 3 },
+    { id: 4, tittle: "livre 4", authorId: 4, categoryId: 4 },
+]
+
+app.get('/books', async (req, res) => {
+    res.json(books)
+})
+
+app.get('/books/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const book = books.find(book => book.id === id)
+    if (book) {
+        try {
+            const authorResponse = await axios.get(`http://localhost:4000/authors/${book.authorId}`)
+            const categoryResponse = await axios.get(`http://localhost:5000/categories/${book.categoryId}`)
+            const author = authorResponse.data
+            const category = categoryResponse.data
+
+            bookDetails = {
+                id: book.id,
+                tittle: book.tittle,
+                author: author.name,
+                category: category.name
+            }
+
+            res.json(bookDetails)
+
+        } catch (error) {
+            res.status(500).json({ error: "dsfsd" })
+        }
+    } else {
+        res.status(500).json({ error: "dsfffffsd" })
+    }
+})
+
+app.delete('/books/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = books.findIndex(book => book.id === id)
+    if (index === -1) {
+        res.status(404).json({ error: "not found" })
+    }
+
+    books.splice(index, 1)
+    res.json(books)
+
+})
+
+app.put('/books/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = books.findIndex(book => book.id === id)
+    const book = books.find(book => book.id === id)
+    if (book) {
+        try {
+
+            const newBook = {
+                id: req.body.id,
+                tittle: req.body.tittle,
+                authorId: req.body.authorId,
+                categoryId: req.body.categoryId
+
+            }
+
+            books[index] = newBook
+
+            res.json(books)
+
+        } catch (error) {
+            res.status(500).json({ error: "dsfsd" })
+        }
+    } else {
+        res.status(500).json({ error: "dsfffffsd" })
+    }
+})
+
+app.post('/books', (req, res) => {
+    console.log(req.body)
+    const newBook = {
+        id: req.body.id,
+        tittle: req.body.tittle,
+        authorId: req.body.authorId,
+        categoryId: req.body.categoryId
+
+    }
+    books.push(newBook)
+    res.json(books)
+})
+
+app.listen(3000, () => {
+    console.log(`Server running at 3000`)
+
+})
